cfd795dedb619307bff057ad5f9c1ee22cba1a4e
maheshshinde9100 <maheshshinde9100@gmail.com>
2026-06-29 20:03:49 +0530

    intial server-side commit

---
+++ server/src/main/java/com/mahesh/RedirectX/RedirectXApplication.java
@@ -0,0 +1,13 @@
+package com.mahesh.RedirectX;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedirectXApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedirectXApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/mahesh/RedirectX/RedirectXApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mahesh.RedirectX;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedirectXApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
